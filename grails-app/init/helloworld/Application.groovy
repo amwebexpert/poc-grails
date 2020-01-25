d53b5167ae42ba97002cbc159075c8f60bfb5c24
amwebexpert <amwebexpert@gmail.com>
2020-01-25 16:54:16 -0500

    Added bootstrapping example code

--- grails-app/init/helloworld/Application.groovy
+++ grails-app/init/helloworld/Application.groovy
@@ -2,9 +2,10 @@ package helloworld
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import grails.core.GrailsApplicationLifeCycle
 import org.springframework.context.annotation.Bean
 
-class Application extends GrailsAutoConfiguration {
+class Application extends GrailsAutoConfiguration implements GrailsApplicationLifeCycle {
 
     static void main(String[] args) {
         GrailsApp.run(Application, args)
@@ -12,7 +13,19 @@ class Application extends GrailsAutoConfiguration {
 
     @Bean
     MyBean myBean() {
-        return new MyBean()
+        new MyBean()
+    }
+
+    @Override
+    void onStartup(Map<String, Object> event) {
+        super.onStartup(event)
+        log.info("********** HelloWorld demo APP STARTUP")
+    }
+
+    @Override
+    void onShutdown(Map<String, Object> event) {
+        super.onShutdown(event)
+        log.info("********** HelloWorld demo APP SHUTDOWN")
     }
 
 }
--- grails-app/init/helloworld/BootStrap.groovy
+++ grails-app/init/helloworld/BootStrap.groovy
@@ -1,9 +1,27 @@
 package helloworld
 
+import grails.util.Environment
+
 class BootStrap {
 
-    def init = { servletContext ->
+    def init = { ctx ->
+
+        Environment.executeForCurrentEnvironment {
+            production {
+                // do something in production
+                log.info("Production environnement starting...")
+                ctx.setAttribute("env", "prod")
+            }
+            development {
+                // do something only in development
+                log.info("Development environnement starting...")
+                ctx.setAttribute("env", "dev")
+            }
+        }
+
+        ctx.setAttribute("foo", "bar")
     }
+
     def destroy = {
     }
 }
