76a2ce2f4876c9af7f5644498ba3b54a98a472de
amwebexpert <amwebexpert@gmail.com>
2020-01-26 13:30:28 -0500

    Added json example

--- grails-app/controllers/helloworld/WelcomeController.groovy
+++ grails-app/controllers/helloworld/WelcomeController.groovy
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value
 
 class WelcomeController {
 
+    static responseFormats = ['json', 'html']
+
     // In Groovy code you must use single quotes around the string for the value of the Value annotation otherwise it is
     // interpreted as a GString not a Spring expression.
     @Value('${info.app.internal.counter}')
@@ -67,6 +69,7 @@ class WelcomeController {
     }
 
     // http://localhost:8080/welcome/books.json
+    // http://localhost:8080/welcome/books (because we defined JSON as a priority inside responseFormats at the beginning of the controller class)
     def books() {
         // The respond method is the preferred way to return JSON
         respond bookService.list()
